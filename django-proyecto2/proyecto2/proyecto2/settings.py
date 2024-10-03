bc54148ee679ea51df94eb8222542ed7720c7246
julian-ingen <julian_stone_@hotmail.com>
2024-10-02 21:48:21 -0300

    deploy

--- django-proyecto2/proyecto2/proyecto2/settings.py
+++ django-proyecto2/proyecto2/proyecto2/settings.py
@@ -130,11 +130,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIR ={
+STATICFILES_DIRS={
     os.path.join(BASE_DIR, 'staticfiles')
 }
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'prt/static')
  
 
 # Default primary key field type
